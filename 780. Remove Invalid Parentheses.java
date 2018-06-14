3135cce14049d7270c7ca1cb247445ed3ad279c4
Derek Mao <40018855+DerekMao@users.noreply.github.com>
2018-06-14 11:33:45 +0800

    Update 780. Remove Invalid Parentheses.java

--- 780. Remove Invalid Parentheses.java
+++ 780. Remove Invalid Parentheses.java
@@ -1,54 +1,53 @@
 public class Solution {
     public int removed;
-    /**
-     * @param s: The input string
-     * @return: Return all possible results
-     */
+
     public List<String> removeInvalidParentheses(String s) {
-        Map<Integer, List<String>> map = new HashMap();
-        List<String> list = new ArrayList();
+        List<String> result = new ArrayList();      //存放结果
+        List<String> list = new ArrayList();        //作为传入参数
         StringBuilder sb = new StringBuilder(s);
         removed = 0;
         
-        helper(s, sb, list, map, 0);
-        for (int i = 0; i <= s.length(); i++) {
-            if (map.containsKey(i)) {
-                return map.get(i);
+        if (s.length() == 0 || s == null) {
+            result.add(s);
+            return result;
+        }
+        
+        helper(s, sb, list, 0);
+        int maxlength = -1;                                     //helper过后所有有效组合都存在list里
+        for (String entry: list) {                              //找最大长度（经过最少删除）
+            maxlength = Math.max(maxlength, entry.length());
+        }
+        for (String entry: list) {                              //往result中添加最大长度的组合
+            if (entry.length() == maxlength) {
+                result.add(entry);
             }
         }
-        return list;
+        return result;
     }
     
     private void helper(String s,
                         StringBuilder sb,
                         List<String> list,
-                        Map<Integer, List<String>> map,
                         int index) {
-        
         if (index >= sb.length()) {
             return;
         }
-        if (sb.charAt(index) != '(' && sb.charAt(index) != ')') {
-            return;
-        }
         for (int i = index; i < sb.length(); i++) {
-            if (i > index && sb.charAt(i) == sb.charAt(i - 1)){
+            if (i > index && sb.charAt(i) == sb.charAt(i - 1)){ //去重
                 continue;
             }
             sb.deleteCharAt(i);
             removed++;
             if (isValid(sb)) {
                 list.add(sb.toString());
-                map.put(removed, list);
             }
-            helper(s, sb, list, map, i);
-            sb.insert(i, s.charAt(i + removed - 1));
-            removed--;
-            list = new ArrayList();
+            helper(s, sb, list, i);                             //此处依旧传i，因为删除后，后一个char跑到了i位置上
+            sb.insert(i, s.charAt(i + removed - 1));            //回溯
+            removed--;                                          //回溯
         }
     }
     
-    private boolean isValid(StringBuilder str) {
+    private boolean isValid(StringBuilder str) {    
         if (str == null && str.length() == 0) {
             return true;
         }
